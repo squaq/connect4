8eff1119404bcf19a64547cfdfacde4dddf61676
TissTech - Danilo S <tisstech@Mac-mini-de-TissTech.local>
2016-07-13 17:49:23 -0300

    second commit 2

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+(function( $, window ){
+    window.App = {};
+    App = {
+        init:function(){
+            var ww = $(window).width(),
+                wh = $(window).height(),
+                renderer = PIXI.autoDetectRenderer()
+        }    
+    };
+    
+    App.init();
+}(jQuery, this));
